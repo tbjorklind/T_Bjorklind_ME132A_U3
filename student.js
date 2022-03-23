0e60079db7b7f6dc9fa81e2850a06d39f4998f28
tbjorklind <97900554+tbjorklind@users.noreply.github.com>
2022-03-23 10:31:01 +0100

    Added js-file

---
+++ student.js
@@ -0,0 +1,6 @@
+// Förnamn: första studenten.
+// console.log(DATABASE.students[0].firstName)
+// // Efternamn: första studenten.
+// console.log(DATABASE.students[0].lastName)
+// // Credits: första kursen, behövs loop för att räkna ut total credit.
+// console.log(DATABASE.students[0].courses[0].passedCredits)
