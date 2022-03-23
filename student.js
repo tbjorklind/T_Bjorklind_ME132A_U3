91250a3eba6e51256517bc68cf51a3951cdcb944
tbjorklind <97900554+tbjorklind@users.noreply.github.com>
2022-03-23 14:15:16 +0100

    Added new functions

--- student.js
+++ student.js
@@ -1,6 +1,36 @@
-// Förnamn: första studenten.
-// console.log(DATABASE.students[0].firstName)
-// // Efternamn: första studenten.
-// console.log(DATABASE.students[0].lastName)
-// // Credits: första kursen, behövs loop för att räkna ut total credit.
-// console.log(DATABASE.students[0].courses[0].passedCredits)
+let input = document.getElementById('student-input')
+
+function getStudent () {
+  let student = DATABASE.students
+    .filter((student) => student.lastName.toLowerCase().includes(input.value))
+    .map((student) => student.firstName + ' ' + student.lastName)
+  return student
+}
+
+input.addEventListener('keyup', function () {
+  let gotStudent = getStudent()
+  let studentContainer = document.getElementById('student-container')
+  studentContainer.innerHTML = ''
+  runCreateStudentBox(gotStudent)
+
+  if (input.value == 0) {
+    studentContainer.innerHTML = ''
+  }
+})
+
+function createStudentBox (student) {
+  let studentContainer = document.getElementById('student-container')
+  let studentDiv = document.createElement('div')
+  studentDiv.classList.add('student-box')
+  studentDiv.innerHTML = `
+<h1 class="student-name">${student} (total: ${'hej'} credits)</h1>
+<h2>Courses:</h2>
+`
+  studentContainer.appendChild(studentDiv)
+}
+
+function runCreateStudentBox (students) {
+  for (let student of students) {
+    createStudentBox(student)
+  }
+}
